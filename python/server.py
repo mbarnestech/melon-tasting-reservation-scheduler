b41f08a2677c567da657ef4f73b83c3b66048305
Maggie <86427114+mbarnestech@users.noreply.github.com>
2023-09-26 13:47:36 -0600

    Set Up Server (#1)
    Set up simple Flask Server with test endpoint; set up endpoint to run in
    Postman.

---
+++ python/server.py
@@ -0,0 +1,35 @@
+
+# import Python modules
+from dotenv import load_dotenv
+from flask import Flask
+from os import environ
+#---------------------------------------------------------------------#
+
+# create Flask app
+app = Flask(__name__)
+
+# Load environment variables
+load_dotenv()
+
+# Create secret key to use Flask session feature
+app.secret_key = environ['APPSECRETKEY']
+
+# Make the Flask interactive debugger better in testing 
+# TODO - remove before deployment
+app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = True
+
+#---------------------------------------------------------------------#
+
+@app.route("/test")
+def index():
+    """Return test."""
+    print("Testing Successful")
+    return 'Successful Test'
+
+#---------------------------------------------------------------------#
+
+# when this file is run
+if __name__ == "__main__":
+    # run server
+    app.run(debug=True, host="0.0.0.0")
+
